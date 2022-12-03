47c2ebbae091b8e3d2da3d0f556f4bbedbd52bbe
erickelsas <erickelsas@alunos.utfpr.edu.br>
2022-12-03 15:10:25 -0300

    teste aws

--- src/context/RoutesContext.js
+++ src/context/RoutesContext.js
@@ -3,10 +3,10 @@ import { createContext } from "react";
 export const RoutesContext = createContext();
 
 export const RoutesContextProvider = ({children}) => {
-    const urlGeral = 'https://agile-sierra-87364.herokuapp.com/http://fomev10-env.eba-sq3qqxd7.sa-east-1.elasticbeanstalk.com';
+    const urlGeral = 'http://fomev10-env.eba-sq3qqxd7.sa-east-1.elasticbeanstalk.com';
 
     const urls = {
-        geral: 'https://agile-sierra-87364.herokuapp.com/http://fomev10-env.eba-sq3qqxd7.sa-east-1.elasticbeanstalk.com',
+        geral: 'http://fomev10-env.eba-sq3qqxd7.sa-east-1.elasticbeanstalk.com',
         product: `${urlGeral}/product/`,
         user: `${urlGeral}/user/`,
         order: `${urlGeral}/order/`,
