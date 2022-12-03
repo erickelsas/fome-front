f6f0ddae03916c8f9fc7bc2c7479bc75d3711cb6
erickelsas <erickelsas@alunos.utfpr.edu.br>
2022-12-03 18:56:12 -0300

    renda teste

--- src/pages/admin-receita/AdminRevenue.js
+++ src/pages/admin-receita/AdminRevenue.js
@@ -1,11 +1,61 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import SideMenu from '../../components/SideMenu';
+import { RoleContext } from '../../context/RoleContext';
+import { RoutesContext } from '../../context/RoutesContext';
 
 import './AdminRevenue.css';
 
 const AdminRevenue = () => {
     const [filter, setFilter] = useState('mês');
+    const [ loading, setLoading] = useState(false);
+
+    const date = new Date();
+    const today = date.getDate();
+    const currentMonth = date.getMonth() + 1;
+    const currentYear = date.getFullYear();
+
+    const finalDate = `${currentYear}-${currentMonth}-${today}`;
+
+    const [value, setValue] = useState(0);
+
+    const { urls } = useContext(RoutesContext);
+    const { token } = useContext(RoleContext);
+
+    useEffect(() => {
+        const fetchValue = async () => {
+            const filterDate = () => {
+                setLoading(true);
+
+                if(filter === 'mês'){
+                    return `${currentYear}-${currentMonth-1}-${today}`
+                }
+    
+                if(filter === 'semestre'){
+                    return `${currentYear}-${currentMonth-6}-${today}`
+                }
+            
+                if(filter === 'ano'){
+                    return `${currentYear-1}-${currentMonth-1}-${today}`
+                }
+            }
+     
+            const objDate = {
+                iniDate: filterDate(),
+                finalDate,
+            }
+
+            const res = await fetch(`${urls.table}balance-sheet`, {method: "POST", headers:{"Content-Type":"application/json", "Authorization": `Bearer ${token}`}, body: JSON.stringify(objDate)});
+            const json = await res.json();
+
+            setValue(json.result);
+
+            setLoading(false);
+        }
+
+        fetchValue();
+    }, [filter, currentMonth, currentYear, finalDate, today, urls.table, token]);
+
   return (
     <div className='admin-revenue'>
         <SideMenu page='receita'/>
@@ -17,18 +67,18 @@ const AdminRevenue = () => {
                     <div className="filtro">
                         <button 
                             className={filter === 'mês' ? "filter-option active" : "filter-option"} 
-                            onClick={() => setFilter('mês')}>
+                            onClick={() => {setFilter('mês');}}>
                                 mês</button>
                         <button 
                             className={filter === 'semestre' ? "filter-option active" : "filter-option"} 
-                            onClick={() => setFilter('semestre')}>
+                            onClick={() => {setFilter('semestre');}}>
                                 semestre</button>
                         <button 
                             className={filter === 'ano' ? "filter-option active" : "filter-option"} 
-                            onClick={() => setFilter('ano')}>
+                            onClick={() => {setFilter('ano');}}>
                                 ano</button>
                     </div>
-                    <h2 className='display-3 valor'>R$10.000,00</h2>
+                    <h2 className='display-3 valor'>R${value},00</h2>
                 </div>
                 <div className='grafico'>
 
